972626690901ad6c3d0c71f7e4775a29cfd3b19b
Luvia6002 <mchluvia@gmail.com>
2025-02-09 22:08:50 -0600

    Secretary.py

--- Secretary.py
+++ Secretary.py
@@ -1,4 +1,5 @@
 from Person import Person
+
 class Secretary(Person):
     def __init__(self, paramName: str, paramTarjet: int, paramWork: str, paramAvailableOn: str, paramPlace: str):
         super().__init__(paramName, paramTarjet, paramWork, paramAvailableOn, paramPlace)
